987d4c8a3f89d5471549529ad62bedf0cab91d27
Alexander Franco <alexfranco12@gmail.com>
2022-07-27 09:46:44 -0500

    add environment variables

--- RubyLinuxClient/server.rb
+++ RubyLinuxClient/server.rb
@@ -1,15 +1,17 @@
 require 'sinatra'
+set :opentok_api_key, ENV['OPENTOK_API_KEY']
 
 get '/' do
   "Hello World"
+  settings.opentok_api_key
 end
 
 post '/start/:sessionId/:token' do
   @sessionId = params["sessionId"]
-  @token = params[token]
+  @token = params["token"]
 
   publisher = Process.spawn('./linux-publisher/src/build/headless-video-publisher', [
-      '-v', 'video.yuv', '-a', 'audio.pcm', `-k`, process.env.OPENTOK_API_KEY, `-s`, @sessionId, `-t`, @token
+      '-v', 'video.yuv', '-a', 'audio.pcm', `-k`, settings.opentok_api_key, `-s`, @sessionId, `-t`, @token
     ])
   Process.wait publisher
 
